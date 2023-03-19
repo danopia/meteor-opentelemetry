2877caf30bf7e707b3562d0d72716f9d82e1b294
Daniel Lamando <dan@danopia.net>
2023-03-20 00:14:05 +0100

    Some slight DDP span cleanup

--- instrument/ddp-client.ts
+++ instrument/ddp-client.ts
@@ -5,7 +5,8 @@ import { context, propagation, SpanKind, trace } from "@opentelemetry/api";
 
 // We hook the DDP apply
 
-const Mtracer = trace.getTracer('ddp.method');
+const methodTracer = trace.getTracer('ddp.method');
+const subTracer = trace.getTracer('ddp.subscription');
 
 
 const ddp = Meteor.connection as ReturnType<typeof DDP.connect>;
@@ -25,7 +26,7 @@ console.log('welcome', ddp.status())
 // }
 const origApply = ddp._apply
 ddp._apply = function _apply(this: typeof ddp, name, stubCallValue, args, options, callback) {
-  return Mtracer.startActiveSpan(name, {
+  return methodTracer.startActiveSpan(name, {
     kind: SpanKind.CLIENT,
     attributes: {
       'rpc.system': 'ddp',
@@ -40,6 +41,48 @@ ddp._apply = function _apply(this: typeof ddp, name, stubCallValue, args, option
     origApply.call(this, name, stubCallValue, args, options, callback);
     // span.end();
   });
+};
+
+const origSend = ddp._send
+ddp._send = function _send(this: typeof ddp, payload) {
+  if (payload.msg !== 'sub') {
+    return origSend.call(this, payload);
+  }
+  // console.log('innft', payload)
+
+  const span = subTracer.startSpan(payload.name, {
+    kind: SpanKind.CLIENT,
+    attributes: {
+      'rpc.system': 'ddp-subscribe',
+      'rpc.method': payload.name,
+      'rpc.ddp.session': this.id,
+      'rpc.ddp.version': this.version,
+      'rpc.ddp.sub_id': payload.id,
+      // 'ddp.user_id': this.userId ?? '',
+      // 'ddp.connection': this.connection?.id,
+    },
+  }, context.active());
+  this._subscriptions[payload.id].otelSpan = span;
+
+  const origCallback = this._subscriptions[payload.id].readyCallback;
+  this._subscriptions[payload.id].readyCallback = () => {
+    span.end();
+    origCallback?.();
+  }
+  const origErrCallback = this._subscriptions[payload.id].errorCallback;
+  this._subscriptions[payload.id].errorCallback = (err) => {
+    span.recordException(err);
+    span.end();
+    origErrCallback?.(err);
+  }
+
+  const baggage: Record<string,string> = {};
+  propagation.inject(trace.setSpan(context.active(), span), baggage, {
+    set: (h, k, v) => h[k] = typeof v === 'string' ? v : String(v),
+  });
+  payload.baggage = baggage;
+
+  origSend.call(this, payload);
 };
 
 const origMethodInvokers = ddp._methodInvokers;
--- instrument/ddp-server.ts
+++ instrument/ddp-server.ts
@@ -1,36 +1,65 @@
-import { trace, propagation, ROOT_CONTEXT, SpanKind, SpanStatusCode } from "@opentelemetry/api";
+import { trace, propagation, ROOT_CONTEXT, SpanKind, SpanStatusCode, Span } from "@opentelemetry/api";
 import { Meteor } from "meteor/meteor";
 
 { // Hook receiving rpcs from the client
   const {protocol_handlers} = MeteorX.Session.prototype;
-  const originMethod = protocol_handlers.method;
-  const methodtracer = trace.getTracer('ddp.method')
-  protocol_handlers.method = function (payload, unblock) {
-    if (payload.msg == 'method') {
-      const ctx = propagation.extract(ROOT_CONTEXT, payload.baggage ?? {}, {
-        get(h,k) { return h[k]; },
-        keys(h) { return Object.keys(h); },
-      });
 
-      return methodtracer.startActiveSpan(payload.method, {
-        kind: SpanKind.SERVER,
-        attributes: {
-          'rpc.system': 'ddp',
-          'rpc.method': payload.method,
-          'rpc.ddp.session': this.id,
-          'rpc.ddp.version': this.version,
-          'rpc.ddp.method_id': payload.id,
-          'meteor.user_id': this.userId,
-          'net.peer.name': this.socket.remoteAddress,
-          'net.peer.port': this.socket.remotePort,
-          'net.host.name': this.socket.address.address,
-          'net.host.port': this.socket.address.port,
-          'net.sock.family': ({'IPv4':'inet','IPv6':'inet6'})[this.socket.address.family] ?? this.socket.address.family,
-        },
-      }, ctx, () => originMethod.call(this, payload, unblock));
-    } else {
-      return originMethod.call(this, payload, unblock);
-    }
+  const methodtracer = trace.getTracer('ddp.method');
+  const originMethod = protocol_handlers.method;
+  protocol_handlers.method = function (payload, unblock) {
+    const ctx = propagation.extract(ROOT_CONTEXT, payload.baggage ?? {}, {
+      get(h,k) { return h[k]; },
+      keys(h) { return Object.keys(h); },
+    });
+
+    return methodtracer.startActiveSpan(payload.method, {
+      kind: SpanKind.SERVER,
+      attributes: {
+        'rpc.system': 'ddp',
+        'rpc.method': payload.method,
+        'rpc.ddp.session': this.id,
+        'rpc.ddp.version': this.version,
+        'rpc.ddp.method_id': payload.id,
+        'meteor.user_id': this.userId,
+        'net.peer.name': this.socket.remoteAddress,
+        'net.peer.port': this.socket.remotePort,
+        'net.host.name': this.socket.address.address,
+        'net.host.port': this.socket.address.port,
+        'net.sock.family': ({'IPv4':'inet','IPv6':'inet6'})[this.socket.address.family] ?? this.socket.address.family,
+      },
+    }, ctx, () => originMethod.call(this, payload, unblock));
+  };
+
+  const subtracer = trace.getTracer('ddp.subscription');
+  const origSub = protocol_handlers.sub;
+  protocol_handlers.sub = function (payload, unblock) {
+    const ctx = propagation.extract(ROOT_CONTEXT, payload.baggage ?? {}, {
+      get(h,k) { return h[k]; },
+      keys(h) { return Object.keys(h); },
+    });
+
+    this.subSpans ??= new Map();
+
+    return subtracer.startActiveSpan(payload.name, {
+      kind: SpanKind.SERVER,
+      attributes: {
+        'rpc.system': 'ddp-subscribe',
+        'rpc.method': payload.name,
+        'rpc.ddp.session': this.id,
+        'rpc.ddp.version': this.version,
+        'rpc.ddp.sub_id': payload.id,
+        'meteor.user_id': this.userId,
+        'net.peer.name': this.socket.remoteAddress,
+        'net.peer.port': this.socket.remotePort,
+        'net.host.name': this.socket.address.address,
+        'net.host.port': this.socket.address.port,
+        'net.sock.family': ({'IPv4':'inet','IPv6':'inet6'})[this.socket.address.family] ?? this.socket.address.family,
+      },
+    }, ctx, span => {
+      this.subSpans.set(payload.id, span);
+      // console.log('storing', payload.id)
+      return origSub.call(this, payload, unblock);
+    });
   };
 }
 
@@ -43,31 +72,51 @@ import { Meteor } from "meteor/meteor";
     if (payload.msg == 'result') {
       const currentSpan = trace.getActiveSpan();
       if (currentSpan) {
-        if (payload.error instanceof Meteor.Error) {
-          currentSpan.setAttributes({
-            'rpc.ddp.error': payload.error.error,
-            'rpc.ddp.error_reason': payload.error.reason,
-            'rpc.ddp.error_details': payload.error.details,
-          });
-          currentSpan.setStatus({
-            code: SpanStatusCode.ERROR,
-            message: payload.error.message,
-          });
-        } else if (payload.error instanceof Error) {
-          currentSpan.recordException(payload.error);
-          currentSpan.setStatus({
-            code: SpanStatusCode.ERROR,
-            message: payload.error.message,
-          });
-        } else if (payload.error?.message) {
-          currentSpan.setStatus({
-            code: SpanStatusCode.ERROR,
-            message: payload.error.message,
-          });
-        }
+        recordSpanError(currentSpan, payload.error);
         currentSpan.end();
+      }
+    } else if (payload.msg == 'ready') {
+      for (const subId of payload.subs) {
+        const subSpan = this.subSpans?.get(subId);
+        if (subSpan) {
+          // console.log('sssub', subId, !!subSpan);
+          subSpan.end();
+        }
+      }
+    } else if (payload.msg == 'nosub') {
+      const subId = payload.id;
+      const subSpan = this.subSpans?.get(subId);
+      if (subSpan) {
+        // console.log('nosssub', subId, !!subSpan, payload.error);
+        recordSpanError(subSpan, payload.error);
+        subSpan.end();
       }
     }
     return origSend.call(this, payload, ...x);
+  }
+}
+
+function recordSpanError(currentSpan: Span, error: unknown) {
+  if (error instanceof Meteor.Error) {
+    currentSpan.setAttributes({
+      'rpc.ddp.error': error.error,
+      'rpc.ddp.error_reason': error.reason,
+      'rpc.ddp.error_details': error.details,
+    });
+    currentSpan.setStatus({
+      code: SpanStatusCode.ERROR,
+      message: error.message,
+    });
+  } else if (error instanceof Error) {
+    currentSpan.recordException(error);
+    currentSpan.setStatus({
+      code: SpanStatusCode.ERROR,
+      message: error.message,
+    });
+  } else if (error?.message) {
+    currentSpan.setStatus({
+      code: SpanStatusCode.ERROR,
+      message: error.message,
+    });
   }
 }
