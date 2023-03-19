7bb885339ccdeb7706acf4736ce44bd0552084c0
Daniel Lamando <dan@danopia.net>
2023-03-20 00:36:25 +0100

    Version 0.1.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'danopia:opentelemetry',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Meteor instrumentations for OpenTelemetry tracing',
   git: 'https://github.com/danopia/meteor-opentelemetry',
   documentation: 'README.md',
