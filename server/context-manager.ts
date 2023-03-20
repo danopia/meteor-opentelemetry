d96fc5eede1511d2d9ca97cc0d90c8d30bb9cbb5
Daniel Lamando <daniel.lamando@forto.com>
2023-03-21 00:33:13 +0100

    Use stack for context when outside fiber

--- server/context-manager.ts
+++ server/context-manager.ts
@@ -11,7 +11,11 @@ export class MeteorContextManager extends AbstractAsyncHooksContextManager imple
     this.envVar = new Meteor.EnvironmentVariable<Context>();
   }
 
+  // Use stack when we are called outside Meteor
+  public _currentContext = ROOT_CONTEXT;
+
   active(): Context {
+    if (!Fiber.current) return this._currentContext;
     return this.envVar.getOrNullIfOutsideFiber() ?? ROOT_CONTEXT;
   }
   with<A extends unknown[], F extends (...args: A) => ReturnType<F>>(
@@ -22,11 +26,16 @@ export class MeteorContextManager extends AbstractAsyncHooksContextManager imple
   ): ReturnType<F> {
     const cb = thisArg == null ? fn : fn.bind(thisArg);
     if (!Fiber.current) {
-      // We just tolerate running without fiber.
-      // This happens on async operations within general-audience NodeJS libraries.
-      // Maybe a metric could be added so we could drive this to zero, but a general fix isn't clear.
-      // The downside is that spans happening outside a fiber simply aren't observable.
-      return cb(...args);
+      // Since we aren't in a Fiber, we can trust that the user code won't block us
+      // So we just use the sync stack to keep context
+      // This kicks in when spans get out into internal NPM packages
+      const previousContext = this._currentContext;
+      this._currentContext = context || ROOT_CONTEXT;
+      try {
+        return cb(...args);
+      } finally {
+        this._currentContext = previousContext;
+      }
     }
     return this.envVar.withValue(context, () => cb(...args));
   }
