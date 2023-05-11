2781692bfb0beece390c6c616870ea4df9242337
Daniel Lamando <dan@danopia.net>
2023-05-11 07:06:01 +0200

    Implement OTLP/v1/metrics DDP proxy method
    This lets clients submit metrics. More for completeness because I don't
    imagine OpenTelemetry's metrics system being that useful in a browser.

--- ddp-otlp-server.ts
+++ ddp-otlp-server.ts
@@ -2,15 +2,25 @@ import { Meteor } from "meteor/meteor";
 import { check } from "meteor/check";
 
 import { Resource } from '@opentelemetry/resources';
-import { IExportTraceServiceRequest, IKeyValue } from '@opentelemetry/otlp-transformer';
+import {
+  IExportTraceServiceRequest,
+  IExportMetricsServiceRequest,
+  IKeyValue,
+  IResource,
+} from '@opentelemetry/otlp-transformer';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { sendWithHttp } from '@opentelemetry/otlp-exporter-base';
 
 import { settings } from "./settings";
 
 if (settings.enabled) {
+  // These are really only used for their URL, because we receive pre-transformed payloads
   const tracesExporter = new OTLPTraceExporter({
-    url: settings.otlpEndpoint ? `${settings.otlpEndpoint}/v1/traces` : null,
+    url: settings.otlpEndpoint ? `${settings.otlpEndpoint}/v1/traces` : void 0,
+  });
+  const metricsExporter = new OTLPMetricExporter({
+    url: settings.otlpEndpoint ? `${settings.otlpEndpoint}/v1/metrics` : void 0,
   });
 
   const clientResources = new Resource(settings.clientResourceAttributes ?? {});
@@ -20,22 +30,39 @@ if (settings.enabled) {
     value: { stringValue: `${x[1]}` },
   }));
 
+  function mangleResource(x: {resource?: IResource}) {
+    x.resource ??= { attributes: [], droppedAttributesCount: 0 };
+    x.resource.attributes = [
+      { key: 'session.public_ip', value: { stringValue: this.connection?.clientAddress } },
+      ...clientAttributeList,
+      ...x.resource.attributes.filter(x => x.key !== 'service.name'),
+    ];
+  }
+
   Meteor.methods({
-    async 'OTLP/v1/traces'(tracePayload: IExportTraceServiceRequest) {
-      check(tracePayload, {
+
+    async 'OTLP/v1/traces'(payload: IExportTraceServiceRequest) {
+      check(payload, {
         resourceSpans: Array,
       });
-      for (const x of tracePayload.resourceSpans) {
-        x.resource ??= { attributes: [], droppedAttributesCount: 0 };
-        x.resource.attributes = [
-          { key: 'session.public_ip', value: { stringValue: this.connection?.clientAddress } },
-          ...clientAttributeList,
-          ...x.resource.attributes.filter(x => x.key !== 'service.name'),
-        ];
-      }
+      payload.resourceSpans?.forEach(mangleResource);
       await new Promise<void>((ok, fail) =>
-        sendWithHttp(tracesExporter, JSON.stringify(tracePayload), 'application/json', ok, fail));
+        sendWithHttp(tracesExporter, JSON.stringify(payload), 'application/json', ok, fail));
     },
+
+    async 'OTLP/v1/metrics'(payload: IExportMetricsServiceRequest) {
+      check(payload, {
+        resourceMetrics: Array,
+      });
+      payload.resourceMetrics?.forEach(mangleResource);
+      await new Promise<void>((ok, fail) =>
+        sendWithHttp(metricsExporter._otlpExporter, JSON.stringify(payload), 'application/json', ok, fail));
+    },
+
+    async 'OTLP/v1/logs'(payload: IExportMetricsServiceRequest) {
+      // TODO: blocked on https://github.com/open-telemetry/opentelemetry-js/pull/3764
+    },
+
   });
 
 } else {
