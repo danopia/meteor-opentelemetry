171da19e8daa16cfbe41d23de6d966abd5b69311
Daniel Lamando <dan@danopia.net>
2023-09-03 16:16:56 +0200

    Publish 0.4.3 with dep updates

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'danopia:opentelemetry',
-  version: '0.4.2',
+  version: '0.4.3',
   summary: 'Meteor instrumentations for OpenTelemetry tracing',
   git: 'https://github.com/danopia/meteor-opentelemetry',
   documentation: 'README.md',
@@ -10,18 +10,18 @@ Npm.depends({
 
   '@opentelemetry/api': '1.4.1',
 
-  '@opentelemetry/sdk-trace-node': '1.10.0',
-  '@opentelemetry/sdk-trace-web': '1.10.0',
-  '@opentelemetry/semantic-conventions': '1.10.0',
+  '@opentelemetry/sdk-trace-node': '1.15.2',
+  '@opentelemetry/sdk-trace-web': '1.15.2',
+  '@opentelemetry/semantic-conventions': '1.15.2',
 
-  '@opentelemetry/otlp-transformer': '0.36.0',
-  '@opentelemetry/exporter-trace-otlp-http': '0.36.0',
-  '@opentelemetry/exporter-metrics-otlp-http': '0.36.0',
+  '@opentelemetry/otlp-transformer': '0.41.2',
+  '@opentelemetry/exporter-trace-otlp-http': '0.41.2',
+  '@opentelemetry/exporter-metrics-otlp-http': '0.41.2',
 
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('2.10.0');
+  api.versionsFrom('2.12');
   api.use('ecmascript');
   api.use('typescript');
   api.use('zodern:types@1.0.9');
